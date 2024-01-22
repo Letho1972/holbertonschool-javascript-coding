b57956a20f36e5a7a27a6a2600fd8f6f4d73bc55
Letho1972 <ericmarescq@gmail.com>
2024-01-22 12:17:20 +0100

    Write a string to a file  Python is cool

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// Write a script that writes a string to a file.
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the content to the file in utf-8
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    // If an error occurred during writing, print the error object
+    console.error(err);
+  } else {
+    // Print a success message
+    console.log('Content has been successfully written to the file.');
+  }
+});
