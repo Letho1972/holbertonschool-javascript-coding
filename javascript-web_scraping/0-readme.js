0119cde089fc92b9d081b20e8ae666b420b2ddd7
Letho1972 <ericmarescq@gmail.com>
2024-01-22 11:47:50 +0100

    Fixed  argv  C is fun

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,9 +3,10 @@
 // Write a script that reads and prints the content of a file.
 
 const fs = require('fs');
+const filePath = process.argv[2];
 
 // Read the content of the file in utf-8
-fs.readFile('cisfun', 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     // If an error occurred during reading, print the error object
     console.error(err);
