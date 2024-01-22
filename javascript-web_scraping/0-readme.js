adda5cb4266f2a7cc52fe2826af5349aa956815d
Letho1972 <ericmarescq@gmail.com>
2024-01-22 11:34:00 +0100

    Read and print the content of a file  C is fun

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,21 +4,13 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: node readfile.js <file_path>');
-  process.exit(1);
-}
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
 // Read the content of the file in utf-8
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile('cisfun', 'utf-8', (err, data) => {
   if (err) {
     // If an error occurred during reading, print the error object
-    console.error('Error reading the file:', err);
+    console.error(err);
   } else {
     // Print the content of the file
-    console.log('File content:\n', data);
+    console.log(data);
   }
 });
