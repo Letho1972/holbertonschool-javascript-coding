b3ffa0709132ad49f7d80bb1ff631448d2dd7695
Letho1972 <ericmarescq@gmail.com>
2024-01-23 09:39:59 +0100

    Print the title of a Star Wars movie

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,40 +1,14 @@
 #!/usr/bin/node
 
-// Write a script that prints the title of a Star Wars movie.
+const request = require('request');
+const movieID = process.argv[2];
+const apiURL = `https://swapi-api.hbtn.io/api/films/${movieID}`;
 
-const fetch = require('node-fetch');
-
-// Function to fetch Star Wars movie information by ID
-async function getStarWarsMovieTitle (movieId) {
-  const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const movieData = await response.json();
-
-    if (response.ok) {
-      const { title } = movieData;
-      console.log(`${title}`);
-    } else {
-      console.error(`${response.statusText}`);
-    }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
-// Check if the movie ID is provided as a command-line argument
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-  console.error('Usage: node script.js <movie_id>');
-} else {
-  const movieId = parseInt(args[0]);
-
-  if (isNaN(movieId) || movieId <= 0) {
-    console.error('Invalid movie ID. Please provide a positive integer.');
+request(apiURL, (err, response, body) => {
+  if (err) {
+    console.error(err);
   } else {
-    // Call the function with the provided movie ID
-    getStarWarsMovieTitle(movieId);
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
   }
-}
+});
